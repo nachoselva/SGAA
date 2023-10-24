eb312052b73ddfd1c1007c2871e764778b1b8f34
nachoselva <nacho.selva1993@gmail.com>
2023-10-23 23:01:19 -0300

    transactional handling

---
+++ Backend/SGAA.Api/Middleware/TransactionalAttributte.cs
@@ -0,0 +1,8 @@
+﻿namespace SGAA.Api.Middleware
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    sealed class TransactionalAttribute : Attribute
+    {
+       
+    }
+}
